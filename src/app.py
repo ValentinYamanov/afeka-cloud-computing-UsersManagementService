d115731d1b5f408c2ad991d078157d4e0d20875e
Omri Gilhar <gilharomri@gmail.com>
2021-11-04 14:04:33 +0200

    Implement 2 methods: 	1) Put user 	2) Login

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, Response, request, make_response, jsonify
+import os
 from controllers.user_controller import UserController
 
 
@@ -14,19 +15,17 @@ def user_management() -> Response:
             return jsonify(x)
         if request.method == 'DELETE':
             return jsonify({})
-
     except Exception as ex:
-        return print(ex)
+        return make_response(jsonify('Error: No routing implemented'), 404)
 
 
 @app.route("/users/<email>", methods=['PUT', 'GET'])
 def users(email: str) -> Response:
     try:
         if request.method == 'PUT':
-            return jsonify({})
+            return jsonify(user_controller.login(email=email, password=request.json))
         if request.method == 'GET':
             return jsonify({})
-
     except Exception as ex:
         make_response(jsonify('Error: No routing implemented'), 404)
 
@@ -35,8 +34,10 @@ def users(email: str) -> Response:
 # Todo Get password from query parameters
 def login(email: str) -> Response:
     try:
-        return jsonify({})
-
+        password = request.args.get('password')
+        if not password:
+            raise RuntimeError("No password provided")
+        return jsonify(user_controller.login(email, password))
     except Exception as ex:
         make_response(jsonify('Error: No routing implemented'), 404)
 
@@ -52,4 +53,6 @@ def search() -> Response:
 
 
 if __name__ == '__main__':
+    if not os.getenv('API_URL'):
+        raise RuntimeError('No "API_URL" environment variable found in the system')
     app.run(debug=True, threaded=True, port=5000)
